407203faba36e0bc7b4caf0468ed5fe7f0f47e9b
Giacomo Olgeni <olgeni@olgeni.com>
2011-08-28 14:41:42 +0200

    Remove more trailing whitespace.

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -78,9 +78,9 @@
     if (mode && (mode == newMode)) {
         return;
     }
-	
+
     mode = newMode;
-	
+
 	if ([NSThread isMainThread]) {
 		[self updateIndicators];
 		[self setNeedsLayout];
@@ -97,10 +97,10 @@
 }
 
 - (void)setLabelText:(NSString *)newText {
-	
+
 	if([labelText isEqual:newText])
 		return;
-	
+
 	if ([NSThread isMainThread]) {
 		[self updateLabelText:newText];
 		[self setNeedsLayout];
@@ -117,10 +117,10 @@
 }
 
 - (void)setDetailsLabelText:(NSString *)newText {
-	
+
 	if([detailsLabelText isEqual:newText])
 		return;
-	
+
 	if ([NSThread isMainThread]) {
 		[self updateDetailsLabelText:newText];
 		[self setNeedsLayout];
@@ -138,7 +138,7 @@
 
 - (void)setProgress:(float)newProgress {
     progress = newProgress;
-	
+
     // Update display only if showing the determinate progress view
     if (mode == MBProgressHUDModeDeterminate) {
 		if ([NSThread isMainThread]) {
@@ -180,7 +180,7 @@
     if (indicator) {
         [indicator removeFromSuperview];
     }
-	
+
     if (mode == MBProgressHUDModeDeterminate) {
         self.indicator = [[[MBRoundProgressView alloc] initWithDefaultSize] autorelease];
     }
@@ -191,8 +191,8 @@
 						   initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge] autorelease];
         [(UIActivityIndicatorView *)indicator startAnimating];
 	}
-	
-	
+
+
     [self addSubview:indicator];
 }
 
@@ -256,7 +256,7 @@
 	}
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(deviceOrientationDidChange:)
 												 name:UIDeviceOrientationDidChangeNotification object:nil];
-	
+
 	return me;
 }
 
@@ -276,32 +276,32 @@
 		self.graceTime = 0.0;
 		self.minShowTime = 0.0;
 		self.removeFromSuperViewOnHide = YES;
-		
+
 		self.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
-		
+
         // Transparent background
         self.opaque = NO;
         self.backgroundColor = [UIColor clearColor];
-		
+
         // Make invisible for now
         self.alpha = 0.0;
-		
+
         // Add label
         label = [[UILabel alloc] initWithFrame:self.bounds];
-		
+
         // Add details label
         detailsLabel = [[UILabel alloc] initWithFrame:self.bounds];
-		
+
 		taskInProgress = NO;
 		rotationTransform = CGAffineTransformIdentity;
 		_firstLayout = YES;
-		
+
 		//add the dimming background
 		self._backgroundDimmingView = [[[UIView alloc] initWithFrame:self.bounds] autorelease];
         self._backgroundDimmingView.backgroundColor = [UIColor blackColor];
         self._backgroundDimmingView.alpha = 0.0;
         self._backgroundDimmingView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-		
+
 		self._backgroundDimmingView.userInteractionEnabled = NO;
 		self.userInteractionEnabled = NO;
     }
@@ -310,7 +310,7 @@
 
 - (void)dealloc {
 	[[NSNotificationCenter defaultCenter] removeObserver:self];
-	
+
     [indicator release];
     [label release];
     [detailsLabel release];
@@ -320,12 +320,12 @@
 	[minShowTimer release];
 	[showStarted release];
 	[customView release];
-	
+
 	[_backgroundDimmingView removeFromSuperview];
 	[_backgroundDimmingView release];
 	[_cancelButton removeFromSuperview];
 	[_cancelButton release];
-	
+
     [super dealloc];
 }
 
@@ -333,33 +333,33 @@
 #pragma mark Layout
 
 - (void)layoutSubviews {
-	
+
 	if(useAnimation && !_firstLayout && self.animationTransition != UIViewAnimationTransitionNone)
 	{
 		[UIView beginAnimations:nil context:NULL];
 		[UIView setAnimationDuration:0.30];
-		[UIView setAnimationTransition:self.animationTransition forView:self cache:NO];	
+		[UIView setAnimationTransition:self.animationTransition forView:self cache:NO];
 	}
-	
+
     CGRect frame = self.bounds;
-	
+
     // Compute HUD dimensions based on indicator size (add margin to HUD border)
     CGRect indFrame = indicator.bounds;
     self.width = indFrame.size.width + 2 * MARGIN;
     self.height = indFrame.size.height + 2 * MARGIN;
-	
+
     // Position the indicator
     indFrame.origin.x = floor((frame.size.width - indFrame.size.width) / 2) + self.xOffset;
     indFrame.origin.y = floor((frame.size.height - indFrame.size.height) / 2) + self.yOffset;
     indicator.frame = indFrame;
-	
+
 	CGRect lFrame = CGRectZero;
-	
+
     // Add label if label text was set
     if (nil != self.labelText) {
         // Get size of label text
         CGSize dims = [self.labelText sizeWithFont:self.labelFont];
-		
+
         // Compute label dimensions based on font metrics if size is larger than max then clip the label width
         float lHeight = dims.height;
         float lWidth;
@@ -369,7 +369,7 @@
         else {
             lWidth = frame.size.width - 4 * MARGIN;
         }
-		
+
         // Set label properties
         label.font = self.labelFont;
         label.adjustsFontSizeToFitWidth = NO;
@@ -378,35 +378,35 @@
         label.backgroundColor = [UIColor clearColor];
         label.textColor = [UIColor whiteColor];
         label.text = self.labelText;
-		
+
         // Update HUD size
         if (self.width < (lWidth + 2 * MARGIN))
             self.width = lWidth + 2 * MARGIN;
 
 		self.height = self.height + lHeight + PADDING;
-		
+
         // Move indicator to make room for the label
         indFrame.origin.y -= (floor(lHeight / 2 + PADDING / 2));
         indicator.frame = indFrame;
-		
+
         // Set the label position and dimensions
         lFrame = CGRectMake(floor((frame.size.width - lWidth) / 2) + xOffset,
                                    floor(indFrame.origin.y + indFrame.size.height + PADDING),
                                    lWidth, lHeight);
         label.frame = lFrame;
-		
+
         [self addSubview:label];
     }
 	else
 	{
 		[label removeFromSuperview];
 	}
-	
+
 	// Add details label details text was set
 	if (nil != self.detailsLabelText) {
 		// Get size of label text
 		CGSize dims = [self.detailsLabelText sizeWithFont:self.detailsLabelFont];
-		
+
 		// Compute label dimensions based on font metrics if size is larger than max then clip the label width
 		float lHeight = dims.height;
         float lWidth;
@@ -416,7 +416,7 @@
 		else {
 			lWidth = frame.size.width - 4 * MARGIN;
 		}
-		
+
 		// Set label properties
 		detailsLabel.font = self.detailsLabelFont;
 		detailsLabel.adjustsFontSizeToFitWidth = NO;
@@ -425,33 +425,33 @@
 		detailsLabel.backgroundColor = [UIColor clearColor];
 		detailsLabel.textColor = [UIColor whiteColor];
 		detailsLabel.text = self.detailsLabelText;
-		
+
 		// Update HUD size
 		if (self.width < lWidth + 2 * MARGIN)
 			self.width = lWidth + 2 * MARGIN;
 
 		self.height = self.height + lHeight + PADDING;
-		
+
 		// Move indicator to make room for the new label
 		indFrame.origin.y -= (floor(lHeight / 2 + PADDING / 2));
 		indicator.frame = indFrame;
-		
+
 		// Move first label to make room for the new label
 		lFrame.origin.y -= (floor(lHeight / 2 + PADDING / 2));
 		label.frame = lFrame;
-		
+
 		// Set label position and dimensions
 		CGRect lFrameD = CGRectMake(floor((frame.size.width - lWidth) / 2) + xOffset,
 									lFrame.origin.y + lFrame.size.height + PADDING, lWidth, lHeight);
 		detailsLabel.frame = lFrameD;
-		
+
 		[self addSubview:detailsLabel];
 	}
 	else
 	{
 		[detailsLabel removeFromSuperview];
 	}
-	
+
 	if(self.allowsCancelation)
 	{
 		if(!self._cancelButton)
@@ -460,13 +460,13 @@
 			[self._cancelButton setImage:[UIImage imageNamed:@"CloseButton.png"] forState:UIControlStateNormal];
 			[self._cancelButton addTarget:self action:@selector(cancel) forControlEvents:UIControlEventTouchUpInside];
 		}
-		
+
 		self._cancelButton.frame = CGRectMake(((self.bounds.size.width - self.width) / 2) + self.xOffset - 12,
 										 ((self.bounds.size.height - self.height) / 2) + self.yOffset - 12, 29, 29);
-		
+
 		if(![self._cancelButton superview])
             [self addSubview:self._cancelButton];
-		
+
 	}
 	else
 	{
@@ -476,12 +476,12 @@
 			self._cancelButton = nil;
 		}
 	}
-	
+
 	if(useAnimation && !_firstLayout && self.animationTransition != UIViewAnimationTransitionNone)
 	{
 		[UIView commitAnimations];
 	}
-	
+
 	_firstLayout = NO;
 }
 
@@ -490,7 +490,7 @@
 - (void)didMoveToSuperview
 {
 	if(!self._backgroundDimmingView.superview)
-		[self.superview insertSubview:self._backgroundDimmingView belowSubview:self];		
+		[self.superview insertSubview:self._backgroundDimmingView belowSubview:self];
 }
 
 - (void)removeFromSuperview
@@ -513,7 +513,7 @@
 
 - (void)show:(BOOL)animated {
 	useAnimation = animated;
-	
+
 	// If the grace time is set postpone the HUD display
 	if (self.graceTime > 0.0) {
 		self.graceTimer = [NSTimer scheduledTimerWithTimeInterval:self.graceTime
@@ -531,7 +531,7 @@
 
 - (void)hide:(BOOL)animated {
 	useAnimation = animated;
-	
+
 	// If the minShow time is set, calculate how long the hud was shown,
 	// and postpone the hiding operation if necessary
 	if (self.minShowTime > 0.0 && showStarted) {
@@ -545,7 +545,7 @@
 			return;
 		}
 	}
-	
+
 	// ... otherwise hide the HUD immediately
     [self hideUsingAnimation:useAnimation];
 }
@@ -563,29 +563,29 @@
 }
 
 - (void)showWhileExecuting:(SEL)method onTarget:(id)target withObject:(id)object animated:(BOOL)animated {
-	
+
     methodForExecution = method;
     targetForExecution = [target retain];
     objectForExecution = [object retain];
-	
+
     // Launch execution in new thread
 	taskInProgress = YES;
     [NSThread detachNewThreadSelector:@selector(launchExecution) toTarget:self withObject:nil];
-	
+
 	// Show HUD view
 	[self show:animated];
 }
 
 - (void)launchExecution {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
+
     // Start executing the requested task
     [targetForExecution performSelector:methodForExecution withObject:objectForExecution];
-	
+
     // Task completed, update view in main thread (note: view operations should
     // be done only in the main thread)
     [self performSelectorOnMainThread:@selector(cleanUp) withObject:nil waitUntilDone:NO];
-	
+
     [pool release];
 }
 
@@ -595,11 +595,11 @@
 
 - (void)done {
     isFinished = YES;
-	
+
     // If delegate was set make the callback
     self.alpha = 0.0;
 	self._backgroundDimmingView.alpha = 0.0;
-	
+
 	self._backgroundDimmingView.userInteractionEnabled = NO;
 	self.userInteractionEnabled = NO;
 
@@ -608,13 +608,13 @@
 			[delegate performSelector:@selector(hudWasHidden:) withObject:self];
 		}
     }
-	
+
 	if(self._backgroundDimmingView)
     {
         [self._backgroundDimmingView removeFromSuperview];
         self._backgroundDimmingView = nil;
     }
-	
+
 	if (removeFromSuperViewOnHide) {
 		[self removeFromSuperview];
 	}
@@ -622,12 +622,12 @@
 
 - (void)cleanUp {
 	taskInProgress = NO;
-	
+
 	self.indicator = nil;
-	
+
     [targetForExecution release];
     [objectForExecution release];
-	
+
     [self hide:useAnimation];
 }
 
@@ -649,18 +649,18 @@
         if (animationType == MBProgressHUDAnimationZoom) {
             self.transform = rotationTransform;
         }
-		
+
 		self._backgroundDimmingView.alpha = (self.dimBackground ? 0.35:0.0);
-		
+
         [UIView commitAnimations];
-		
+
 		self._backgroundDimmingView.userInteractionEnabled = YES;
 		self.userInteractionEnabled = YES;
     }
     else {
         self.alpha = 1.0;
 		self._backgroundDimmingView.alpha = (self.dimBackground ? 0.35:0.0);
-		
+
 		self._backgroundDimmingView.userInteractionEnabled = YES;
 		self.userInteractionEnabled = YES;
     }
@@ -703,7 +703,7 @@
 
 - (void)fillRoundedRect:(CGRect)rect inContext:(CGContextRef)context {
     float radius = 10.0f;
-	
+
     CGContextBeginPath(context);
     CGContextSetGrayFillColor(context, 0.0, self.opacity);
     CGContextMoveToPoint(context, CGRectGetMinX(rect) + radius, CGRectGetMinY(rect));
@@ -713,7 +713,7 @@
     CGContextAddArc(context, CGRectGetMinX(rect) + radius, CGRectGetMinY(rect) + radius, radius, M_PI, 3 * M_PI / 2, 0);
     CGContextClosePath(context);
     CGContextFillPath(context);
-	
+
 	//now draw the border
 	if(self.drawStroke)
 	{
@@ -747,7 +747,7 @@
 - (void)setTransformForCurrentOrientation:(BOOL)animated {
 	UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
 	NSInteger degrees = 0;
-	
+
 	if (UIInterfaceOrientationIsLandscape(orientation)) {
 		if (orientation == UIInterfaceOrientationLandscapeLeft) { degrees = -90; }
 		else { degrees = 90; }
@@ -755,7 +755,7 @@
 		if (orientation == UIInterfaceOrientationPortraitUpsideDown) { degrees = 180; }
 		else { degrees = 0; }
 	}
-	
+
 	rotationTransform = CGAffineTransformMakeRotation(RADIANS(degrees));
 
 	if (animated) {
@@ -780,16 +780,16 @@
     CGRect allRect = self.bounds;
     CGRect circleRect = CGRectMake(allRect.origin.x + 2, allRect.origin.y + 2, allRect.size.width - 4,
                                    allRect.size.height - 4);
-	
+
     CGContextRef context = UIGraphicsGetCurrentContext();
-	
+
     // Draw background
     CGContextSetRGBStrokeColor(context, 1.0, 1.0, 1.0, 1.0); // white
     CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 0.1); // translucent white
     CGContextSetLineWidth(context, 2.0);
     CGContextFillEllipseInRect(context, circleRect);
     CGContextStrokeEllipseInRect(context, circleRect);
-	
+
     // Draw progress
     float x = (allRect.size.width / 2);
     float y = (allRect.size.height / 2);
